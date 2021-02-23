ae628185e138c0931caceced225359551a69dd74
sunlong01 <sunlong01@zuoyebang.com>
2021-02-23 13:21:28 +0800

    update

--- controller/article/list.go
+++ controller/article/list.go
@@ -4,11 +4,21 @@ import (
 	"github.com/gin-gonic/gin"
 	"ksc/controller"
 	"ksc/model"
+	"strconv"
 )
 
+var limit int = 20
+
 func List(c *gin.Context){
+	page, err := strconv.Atoi(c.DefaultQuery("page", "0"))
+
+	if err != nil {
+		controller.Fail(c, gin.H{}, "error")
+		return
+	}
+
 	article := new(model.Article)
-	list := article.List(0, 20)
+	list := article.List(page, limit)
 	controller.Success(c, gin.H{
 		"list":list,
 	}, "success")
