e51ac78c1ca4efda851658996376e29aeb7575f7
sunlong01 <sunlong01@zuoyebang.com>
2021-02-27 15:45:51 +0800

    update

--- model/article.go
+++ model/article.go
@@ -1,10 +1,14 @@
 package model
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/jinzhu/gorm"
 	"ksc/common"
 	"ksc/entity"
+	"ksc/schema"
+	"ksc/util"
+	"strconv"
 )
 
 type Article struct {
@@ -12,11 +16,28 @@ type Article struct {
 }
 
 // List 列表
-func (a *Article) List(page int, limit int) (data []entity.Article) {
+func (a *Article) List(page int, limit int) (schema []schema.ArticleListRes) {
 	a.Db = common.GetDb()
 	offset := page * limit
+	var data []entity.Article
 	a.Db.Where("status = ?", 1).Order("update_time DESC").Offset(offset).Limit(limit).Find(&data)
-	return data
+	for k, v := range data {
+		//加密sourceId
+		data := util.EncryptAES([]byte(strconv.Itoa(v.ID)))
+		sourceId := string(data[:])
+
+		//处理图片
+		var images []string
+		err := json.Unmarshal([]byte(v.Imgs), &images)
+		fmt.Println(err, images, sourceId, k)
+
+
+		//schema[k].SourceId = sourceId
+		//schema[k].TagName  = v.TagName
+		//schema[k].Name	   = v.Name
+		//schema[k].TagSign  = v.TagId
+	}
+	return schema
 }
 
 // Insert 插入数据
